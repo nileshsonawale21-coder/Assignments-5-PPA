a50f6f6e5919ab988b1901d300a9bda929f4f4f2
nileshsonawale21-coder <nileshsonawale2002@gmail.com>
2026-08-30 14:58:39 +0530

    Add files via upload

---
+++ Multiplication.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    int num1 = 0,num2 = 0,Ans = 0;
+
+    printf("Enter the Fist number : ");
+    scanf("%d",&num1);
+
+    printf("Enter the Secound number : ");
+    scanf("%d",&num2);
+
+    Ans = num1 + num2;
+
+    printf("Multiplication is = %d",Ans);
+
+
+    return 0;
+}
